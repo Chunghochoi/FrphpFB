afaedcb070556fcbe7737ba5776d4f76163d651b
chungg <chungcr7juve@gmail.com>
2025-09-28 18:13:43 +0700

    Remove all files

--- api/get-price.php
+++
@@ -1,6 +0,0 @@
-<?php
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET");
-header('Content-Type: application/json;charset=utf8');
-require $_SERVER['DOCUMENT_ROOT'].'/files/config.php';
-echo $tiengoc;
